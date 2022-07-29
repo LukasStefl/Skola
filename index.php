df1791653d8061e7dc3e1e0b6b7565da1fcde780
LukasStefl <lukas.stefl@student.ossp.cz>
2022-07-29 18:46:53 +0200

    commit

--- index.php
+++ index.php
@@ -10,11 +10,50 @@
     <h1>Projekt</h1>
     <br>
     <?php
-    echo "I am alive";
-    $a = array("honza", "Vaněk", "3");
-    echo "<br>";
-    echo "I like " . $a[0] . ", " . $a[1] . " and " . $a[2] . ".";
-    
+   /* class Human {
+       public $name;
+       public $nationality;
+       public $gender;
+       public $height;
+       public $weight;
+       
+
+        function __construct($name, $gender, $height, $weight, $nationality) {
+            $this->name = $name;
+            $this->nationality = $nationality
+            $this->gender = $gender;
+            $this->height = $height;
+            $this->weight = $weight;
+          }
+          function __destruct() {
+            echo "The human is named {$this->name}, is {$this->nationality = $nationality}, is {$this->gender = $gender}, is {$this->height = $height} tall, weights {$this->weight = $weight}.";
+          }
+        }
+    }*/
+
+    class Human {
+        public $name;
+        public $nationality;
+        public $gender;
+        public $height;
+        public $weight;
+      
+        function __construct($name, $nationality, $gender, $height, $weight) {
+          $this->name = $name;
+          $this->nationality = $nationality;
+          $this->gender = $gender;
+          $this->height = $height;
+          $this->weight = $weight;
+
+        }
+        function __destruct() {
+          echo "The Human is named {$this->name} and he is {$this->nationality} and his gender is {$this->gender}. He is {$this->height} tall and he weights {$this->weight}.";
+        }
+      }
+      
+      $Honza = new Human("Honza", "Czech", "Male", "185cm", "67kg");
+
+
     ?>
 </body>
 </html>
