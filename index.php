d2edb67eb034193a25a33e97d9fb3db54f2be820
LukasStefl <lukas.stefl@student.ossp.cz>
2022-07-29 08:36:08 +0200

    Commit_2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projekt</title>
+</head>
+<body>
+    <h1>Projekt</h1>
+    <br>
+    <?php
+    echo "I am alive";
+    $a = array("honza", "Vaněk", "3");
+    echo "<br>";
+    echo "I like " . $a[0] . ", " . $a[1] . " and " . $a[2] . ".";
+    
+    ?>
+</body>
+</html>
