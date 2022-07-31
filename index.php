5111aa6a7d2a20c727f158b24d4db46a5639b36f
LukasStefl <lukas.stefl@student.ossp.cz>
2022-07-31 14:38:47 +0200

    commit

--- index.php
+++ index.php
@@ -34,12 +34,20 @@
       
       $Honza = new Human("Honza", "Czech", "Male", "185cm", "67kg");
 */
-$cars = array("Volvo", "BMW", "Toyota");
-$arrlength = count($cars);
-
-for($x = 0; $x < $arrlength; $x++) {
-  echo $cars[$x];
-  echo "<br>";
+$cars = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
+    
+for ($row = 0; $row < 4; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  echo "<ul>";
+  for ($col = 0; $col < 3; $col++) {
+    echo "<li>".$cars[$row][$col]."</li>";
+  }
+  echo "</ul>";
 }
 
 
